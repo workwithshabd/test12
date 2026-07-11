b60b4469d133d308c90353dedbe41187c9817d7e
Shabd <workwithshabd@gmail.com>
2026-07-11 15:58:49 +0530

    db connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,14 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
+
+
 
 dotenv.config();
-const PORT= process.env.PORT || 8000;
+const PORT= process.env.PORT ;
 
 app.listen(PORT,()=>{
     console.log(`server is running at port :${PORT}`)
 })
 
+connectDB();
